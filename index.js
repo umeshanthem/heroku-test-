9ce9a58decf0b1ec35db8ef5e29a099e1e39a2d7
umeshanthem <31222264+umeshanthem@users.noreply.github.com>
2017-08-21 16:34:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var add_data = function (line , cb){
+  fs.appendFile("heroku.txt" , line);
+  return cb(null);
+};
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,6 +18,15 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.get('/pro/:id', function(request, response , err) {
+  var id = request.params.id;
+  add_data(id, function(error){
+  
+  });
+  
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
